a4023c0a5f5bbe1dfcb71e30053cd4c1e0ddf1c9
wangfeng <wangfeng@airia.cn>
2023-03-08 18:31:45 +0800

    调整代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+let originPush = VueRouter.prototype.push; //备份原push方法
+
+VueRouter.prototype.push = function (location, resolve, reject) {
+  if (resolve && reject) {
+    //如果传了回调函数，直接使用
+    originPush.call(this, location, resolve, reject);
+  } else {
+    //如果没有传回调函数，手动添加
+    originPush.call(
+      this,
+      location,
+      () => {},
+      () => {}
+    );
+  }
+};
 Vue.use(VueRouter);
 
 const routes = [];
 
 const router = new VueRouter({
-  base: process.env.BASE_URL,
+ 
   routes,
 });
 
